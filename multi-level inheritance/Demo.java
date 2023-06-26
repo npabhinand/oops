07ae74c5a6d65a24165aa1a8e970714d2b47da8b
ABHINAND NARAYANAN <93638081+npabhinand@users.noreply.github.com>
2023-06-26 15:05:53 +0530

    Add files via upload

---
+++ multi-level inheritance/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String args[])
+    {
+        SciCalc obj=new SciCalc();
+        System.out.println(obj.div(5,6));
+        System.out.println(obj.sub(5,6));
+        System.out.println(obj.add(5,6));
+        System.out.println(obj.mul(5,6));
+        System.out.println(obj.power(5,2));
+    }
+    
+}
---
+++ multi-level inheritance/SciCalc.java
@@ -0,0 +1,7 @@
+public class SciCalc extends AdvCalc
+{
+    public double power(int x,int y)
+    {
+        return Math.pow(x, y);
+    }
+}
