9688013d63015bd0d8c6361410349a4b761e9a40
ABHINAND NARAYANAN <93638081+npabhinand@users.noreply.github.com>
2023-06-26 14:54:14 +0530

    Add files via upload

---
+++ AdvCalc.java
@@ -0,0 +1,12 @@
+
+ class AdvCalc extends Calc
+ {
+    public int mul(int x,int y)
+    {
+        return x*y;
+    }
+    public double div(double x,double y)
+    {
+        return x/y;
+    }
+}
---
+++ SimpleInherit.java
@@ -0,0 +1,12 @@
+public class SimpleInherit {
+    public static void main(String args[])
+    {
+        AdvCalc obj=new AdvCalc();
+        System.out.println(obj.div(5,6));
+        System.out.println(obj.sub(5,6));
+        System.out.println(obj.add(5,6));
+        System.out.println(obj.mul(5,6));
+       
+    }
+    
+}
